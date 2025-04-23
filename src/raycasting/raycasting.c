f03dd1e6fa9105f5a5e4562ee30f59676737ceb8
Pierre Carlier <picarlie@c2r1p6.42perpignan.fr>
2025-04-23 18:15:53 +0200

    Added test for step_sign and norm_angle

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: picarlie <picarlie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 13:56:28 by picarlie          #+#    #+#             */
-/*   Updated: 2025/04/23 17:38:27 by picarlie         ###   ########.fr       */
+/*   Updated: 2025/04/23 18:14:41 by picarlie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ int	step_sign(float	angle, char c)
 {
 	if (c == 'x')
 	{
-		if (angle > (M_PI / 2) && angle < (M_PI * 3 / 2))
-			return (-1);
-		else if (angle == (M_PI / 2) || angle == (M_PI * 3 / 2))
+		if (angle == (M_PI / 2) || angle == (M_PI * 3 / 2)) //comparison doesnt seem to work
 			return (0);
+		else if (angle > (M_PI / 2) && angle < (M_PI * 3 / 2))
+			return (-1);
 	}
 	else
 	{
-		if (angle > M_PI)
-			return (-1);
-		else if (!angle || angle == M_PI)
+		if (!angle || angle == M_PI) //comparison doesnt seem to work
 			return (0);
+		else if (angle > M_PI)
+			return (-1);
 	}
 	return (1);
 }
@@ -55,3 +55,16 @@ int	horizon_check(t_ray *ray, char **map, t_player player, t_game game)
 	}
 	return ();
 }
+
+/*Test for step_sign and norm_angle
+int	main ()
+{
+	float	angle;
+
+	angle = 3.9 * M_PI;
+	printf("Angle : %f\n", angle);
+	norm_angle(&angle);
+	printf("Angle norme : %f\n", angle);
+	printf("x : %d\ny : %d\n", step_sign(angle, 'x'), step_sign(angle, 'y'));
+	return (0);
+}*/
