6e51f7863a39a483e7d4e6231b777507b1320c9a
Pierre Carlier <picarlie@c1r3p10.42perpignan.fr>
2025-05-07 17:18:36 +0200

    Added players movements and wall slide

---
+++ src/minilibx/player_movements.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_movements.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: picarlie <picarlie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 17:36:26 by picarlie          #+#    #+#             */
+/*   Updated: 2025/05/07 17:12:00 by picarlie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3D.h"
+
+/* Takes the player structure */
+/* Change the coordinates of the player moving forward */
+void	up_movements(t_player *player, t_map map)
+{
+	int	x;
+	int	y;
+
+	x = floor((*player).p_x + (cos((*player).p_angle) * step_sign((*player).p_angle, x) * playerSpeed));
+	y = floor((*player).p_y + (sin((*player).p_angle) * step_sign((*player).p_angle, y) * playerSpeed));
+	if (!is_wall(x, y, map))
+	{
+		(*player).p_x = x;
+		(*player).p_y = y;
+	}
+	else
+	{
+		if (!is_wall((*player).p_x, y, map))
+			(*player).p_y = y;
+		else if (!is_wall((x, (*player).p_y, map)))
+			(*player).p_x = x;
+	}
+	return ;
+}
+
+/* Takes the player structure */
+/* Change the coordinates of the player moving backward */
+void	down_movements(t_player *player, t_map map)
+{
+	int	x;
+	int	y;
+
+	x = floor((*player).p_x - (cos((*player).p_angle) * step_sign((*player).p_angle, x) * playerSpeed));
+	y = floor((*player).p_y - (sin((*player).p_angle) * step_sign((*player).p_angle, y) * playerSpeed));
+	if (!is_wall(x, y, map))
+	{
+		(*player).p_x = x;
+		(*player).p_y = y;
+	}
+	else
+	{
+		if (!is_wall((*player).p_x, y, map))
+			(*player).p_y = y;
+		else if (!is_wall((x, (*player).p_y, map)))
+			(*player).p_x = x;
+	}
+	return ;
+}
+
+/* Takes the player structure */
+/* Change the coordinates of the player moving to the right */
+void	right_movements(t_player *player, t_map map)
+{
+	int	x;
+	int	y;
+
+	x = floor((*player).p_x + (cos((*player).p_angle + (M_PI / 2)) * step_sign((*player).p_angle, x) * playerSpeed));
+	y = floor((*player).p_y + (sin((*player).p_angle + (M_PI / 2)) * step_sign((*player).p_angle, y) * playerSpeed));
+	if (!is_wall(x, y, map))
+	{
+		(*player).p_x = x;
+		(*player).p_y = y;
+	}
+	else
+	{
+		if (!is_wall((*player).p_x, y, map))
+			(*player).p_y = y;
+		else if (!is_wall((x, (*player).p_y, map)))
+			(*player).p_x = x;
+	}
+	return ;
+}
+
+/* Takes the player structure */
+/* Change the coordinates of the player moving to the left */
+void	left_movements(t_player *player, t_map map)
+{
+	int	x;
+	int	y;
+
+	x = floor((*player).p_x + (cos((*player).p_angle - (M_PI / 2)) * step_sign((*player).p_angle, x) * playerSpeed));
+	y = floor((*player).p_y + (sin((*player).p_angle - (M_PI / 2)) * step_sign((*player).p_angle, y) * playerSpeed));
+	if (!is_wall(x, y, map))
+	{
+		(*player).p_x = x;
+		(*player).p_y = y;
+	}
+	else
+	{
+		if (!is_wall((*player).p_x, y, map))
+			(*player).p_y = y;
+		else if (!is_wall((x, (*player).p_y, map)))
+			(*player).p_x = x;
+	}
+	return ;
+}
+
+// /!\ /!\ /!\ A FAIRE /!\ /!\ /!\
+// Mettre une protection pour les mouvements (check si wall ou obstacle)
+// https://ismailassil.medium.com/ray-casting-c-8bfae2c2fc13 pour les player movement
+// Second, Forward/Backward Movements — it is resolved based on the current position of the player, rotation angle and the up and down direction.
+// The cos(rotAngle) calculate the horizontal component of the movement direction.
+// The sin(rotAngle) calculate the vertical component of the movement direction.
--- src/minilibx/player_rotation.c
+++ src/minilibx/player_rotation.c
@@ -1,18 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minilibx_utils.c                                   :+:      :+:    :+:   */
+/*   player_rotation.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: picarlie <picarlie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/28 17:36:26 by picarlie          #+#    #+#             */
-/*   Updated: 2025/05/07 17:17:13 by picarlie         ###   ########.fr       */
+/*   Created: 2025/05/07 17:11:36 by picarlie          #+#    #+#             */
+/*   Updated: 2025/05/07 17:12:12 by picarlie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D.h"
 
-void init_data(t_data **data)
+/* Takes the player structure */
+/* Change the angle of the player turning to the right */
+void	right_rot_angle(t_player *player)
 {
-	
+	(*player).p_angle += rot_speed;
+	norm_angle((*player).p_angle);
+}
+
+/* Takes the player structure */
+/* Change the angle of the player turning to the left */
+void	left_rot_angle(t_player *player)
+{
+	(*player).p_angle -= rot_speed;
+	norm_angle((*player).p_angle);
 }
