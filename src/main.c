60e0df6656f8b50d46f43f2490dc2f1612190c6a
Wevo-678 <mplbenet@gmail.com>
2025-05-08 17:36:30 +0200

    fenetre

--- src/main.c
+++ src/main.c
@@ -1,48 +1,46 @@
 #include "../includes/cub3D.h"
 
+int close_window(void *param)
+{
+    (void)param;
+    exit(0); // Quitte proprement le programme
+}
+
 int main(int ac, char **av)
 {
-	if (ac != 2)
-	{
-		write(2, "Error\nWrong number of arguments\n", 33);
-		return (1);
-	}
+    if (ac != 2)
+    {
+        write(2, "Error\nWrong number of arguments\n", 33);
+        return (1);
+    }
 
-	// t_data *data;
-	t_map *map;
-	t_texture *texture;
-	// t_game *game;
-	// t_player *player;
-	int fd;
+    t_map *map;
+    t_texture *texture;
+    int fd;
 
+    init_map(&map);
+    init_texture(&texture);
 
-	init_map(&map);
-	init_texture(&texture);
-	// init_data(&data);
-	// init_game(&game);
-	// init_player(&player);
-	// init_ray(&ray);
+    void *mlx;
+    void *mlx_win;
 
+    mlx = mlx_init();
+    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 
+    // Exemple d'utilisation de mlx_win : gestion de la fermeture de la fenêtre
+    mlx_hook(mlx_win, 17, 0, close_window, NULL); // Fermer avec la croix
+    mlx_loop(mlx);
 
-	fd = open_args(ac, av, map);
-	read_args(fd, map);
-	extract_texture(map->buffer, texture);
-	verify_texture(texture);
-	extract_map(map->buffer, map);
-	print_map(map);
-	verify_map(map);
-	parse_map(map);
-	print_map(map);
-	
+    fd = open_args(ac, av, map);
+    read_args(fd, map);
+    extract_texture(map->buffer, texture);
+    verify_texture(texture);
+    extract_map(map->buffer, map);
+    print_map(map);
+    verify_map(map);
+    parse_map(map);
+    print_map(map);
 
-
-	// test
-	// print_texture(*texture);
-	// test 
-	// print_struct(*map);
-	// printf("%s", map->path);
-
-	close(fd);
-	return (0);
+    close(fd);
+    return (0);
 }
